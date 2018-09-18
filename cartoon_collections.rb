8fe496d8802e8c659f4ac4f44a4b4352b12fde3e
Manny Pissanos <mpissano@gmail.com>
2018-09-18 20:01:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,6 @@ end
  
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if (array & cheese_types).length > 0
-    return array & cheese_types[0].join(" ")
+  if (array & cheese_types)
   end
 end
