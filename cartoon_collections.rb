ad9da9c4e3cba8e9fc842804ef991ea21ca522d0
Manny Pissanos <mpissano@gmail.com>
2018-09-18 18:45:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index puts |x|
 end
+  
 
 def summon_captain_planet# code an argument here
   # Your code here
