b873b382da4025401b5879453d8cf07ca6a99923
Manny Pissanos <mpissano@gmail.com>
2018-09-18 19:33:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,8 @@ def long_planeteer_calls(array)
 test_arr << array.select { |num| num.length > 4 } 
  test_arr.length == 0
  end
+ else return false
+ end
 
 # def find_the_cheese# code an argument here
 #   # the array below is here to help
