2e87a59aff6d1b2d7437869c44900dd3a323e14b
Manny Pissanos <mpissano@gmail.com>
2018-09-18 19:45:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,15 +11,10 @@ end
 def long_planeteer_calls(array)
    array.any?{ |calls| calls.length > 4 }
 end
- array.length != 0
- return true
-   end
- end
+
  
  
- 
- 
-# def find_the_cheese# code an argument here
-#   # the array below is here to help
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-# end
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
