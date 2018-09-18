f765abc2d460692c7c713f63513f3bb4493bd343
Manny Pissanos <mpissano@gmail.com>
2018-09-18 20:10:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ end
  
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.select{ |x| cheese_types.include?(x) }
+  array.select{ |x| cheese_types.include?(x) }.join(" ")
 end
